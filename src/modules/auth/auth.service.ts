c8227949be35175c0fc3c840de1ad3e2c0518e21
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-08-29 11:41:42 +0330

    add error messages and user authenticationn

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,21 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { authDto } from './dto/auth.dto';
 import { authType } from './enums/type.enum';
 import { authMethod } from './enums/method.enum';
 import { isEmail, isPhoneNumber } from 'class-validator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { ProlfileEntity } from '../user/entities/profile.entity';
+import { AuthMessage, BadRequestMessage } from './enums/messages.enum';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        @InjectRepository(UserEntity) private userRespository: Repository<UserEntity>,
+        @InjectRepository(ProlfileEntity) private profileRespository: Repository<ProlfileEntity>
+
+    ) { }
     async userExistence(authDto: authDto) {
         const { type, method, username } = authDto
         switch (type) {
@@ -18,12 +28,31 @@ export class AuthService {
         }
     }
     async login(method: authMethod, username: string) {
-        return this.usernameVallidator(method, username)
+        const validUsername = this.usernameVallidator(method, username)
+        const user = await this.checkExistUser(method, validUsername)
+        if (!user) throw new UnauthorizedException(AuthMessage.NotFoundAccoant)
     }
     async register(method: authMethod, username: string) {
-        return this.usernameVallidator(method, username)
+        const validUsername = this.usernameVallidator(method, username)
+        const user = await this.checkExistUser(method, validUsername)
+        if (user) throw new ConflictException(AuthMessage.AlreadyExists)
     }
-
+    async checkOtp() { 
+        
+    }
+    async checkExistUser(method: authMethod, username: string) {
+        let user: UserEntity;
+        if (method === authMethod.Phone) {
+            user = await this.userRespository.findOneBy({ phone: username })
+        } else if (method === authMethod.Email) {
+            user = await this.userRespository.findOneBy({ email: username })
+        } else if (method === authMethod.Username) {
+            user = await this.userRespository.findOneBy({ username: username })
+        } else {
+            throw new BadRequestException(BadRequestMessage.InvalidData)
+        }
+        return user
+    }
     usernameVallidator(method: authMethod, username: string) {
         switch (method) {
             case authMethod.Email:
---
+++ src/modules/auth/enums/messages.enum.ts
@@ -0,0 +1,35 @@
+
+
+export enum BadRequestMessage {
+    InvalidLoginData = 'Invalid login data',
+    InvalidRegisterData = 'Invalid register data',
+    InvalidToken = 'Invalid token',
+    InvalidUser = 'Invalid user',
+    InvalidEmail = 'Invalid email',
+    InvalidUsername = 'Invalid username',
+    InvalidId = 'Invalid id',
+    InvalidData = 'Invalid data',
+    InvalidRole = 'Invalid role',
+    InvalidImage = 'Invalid image',
+    InvalidFile = 'Invalid file',
+    InvalidFileExtension = 'Invalid file extension',
+    InvalidFileType = 'Invalid file type',
+    InvalidFileSize = 'Invalid file size',
+    InvalidFileCount = 'Invalid file count',
+    InvalidFileCountMax = 'Invalid file count max',
+    InvalidFileCountMin = 'Invalid file count min',
+    InvalidFileCountRange = 'Invalid file count range',
+    InvalidFileCountRequired = 'Invalid file count required',
+    InvalidFileCountRequiredMax = 'Invalid file count required max',
+    InvalidFileCountRequiredMin = 'Invalid file count required min',
+    InvalidFileCountRequiredRange = 'Invalid file count required range',
+    InvalidFileCountRequiredRequired = 'Invalid file count required required',
+    InvalidFileCountRequiredRequiredMax = 'Invalid file count required required max',
+    InvalidFileCountRequiredRequiredMin = 'Invalid file count required required min',
+    InvalidFileCountRequiredRequiredRange = 'Invalid file count required required range',
+    InvalidFileCountRequiredRequiredRequired = 'Invalid file count required required required',
+}
+export enum AuthMessage {
+    NotFoundAccoant = "Accoant not found",
+    AlreadyExists = "User already exist",
+}
