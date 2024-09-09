db155a2630c3bd532925648bc8f756dbf98c633c
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-09-09 13:42:51 +0330

    set file size

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { swaggerConfig } from './config/swagger.config';
 import { AppModule } from './modules/app/app.module';
 import * as cookieParser from 'cookie-parser';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   swaggerConfig(app)
   const { PORT,COOKIE_SECRET } = process.env
   app.use(cookieParser(COOKIE_SECRET));
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(PORT, () => {
     console.log(`api: http://localhost:${PORT}`)
     console.log(`swagger: http://localhost:${PORT}/swagger`)
